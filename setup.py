e4419db4e79cc44e5f146327b6bc331cd9f4891d
Iurii Kriachko <yuriykryachko@gmail.com>
2014-06-14 12:01:05 +0200

    add setup.py package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='geodis',
+    version='0.1',
+    description='Geodis - a Redis based geo resolving library',
+    packages=find_packages(),
+    install_requires=[
+        'redis',
+        'geohasher',
+    ],
+)
